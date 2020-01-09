da9a6031dbeb045d9838d10813b7602911463579
divyasri2927 <nethidivyasri@gmail.com>
2020-01-09 06:22:33 +0000

    logic

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 var rsp=require('./roscpa')
 var choice1=prompt("enter your a choice rock/scissors/paper")
-var choice2=prompt("enter your a choice rock/scissors/paper")
+var choice2=Math.random()
+if (choice2 < 0.34){
+	choice2 = "rock";
+} 
+else if(choice2 <= 0.89) {
+	choice2 = "paper";
+} 
+else {
+	choice2 = "scissors";
+} 
+console.log("Computer: " + choice2);
 rsp.roscpa(choice1,choice2)
--- roscpa.js
+++ roscpa.js
@@ -3,11 +3,17 @@ if(ch1==ch2){
   return "its a tie"
 }else if(ch1=="rock" && ch2=="scissors"){
   return "rock is win"
-}else if(ch1=="scissors" && ch2=="paper" ){
-  return "scissors is win"
-}else if(ch1=="paper" && ch2=="rock"){
+}else {
   return "paper is win"
-}else if(ch1!=ch2){
+}if(ch1=="scissors" && ch2=="paper" ){
+  return "scissors is win"
+}else{
+  return "rock is win"
+}if(ch1=="paper" && ch2=="rock"){
+  return "paper is win"
+}else{
+  return "scissors is win"
+}if(ch1!=ch2){
   return "input is wrong"
 }
 }
